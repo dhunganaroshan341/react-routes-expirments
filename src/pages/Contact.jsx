29203e85f79ad3a2236d267c4bde42d2e4f221d7
Roshan Dhungana <dhunganaroshan341@gmail.com>
2026-06-03 15:28:06 +0545

    feat form submission contnact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,99 @@
+import { useState } from "react";
+
 export default function Contact() {
-  return <h2>📩 Contact Page</h2>;
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const [errors, setErrors] = useState({});
+
+  // input handler
+  function handleChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  // submit handler
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let newErrors = {};
+
+    if (!form.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!form.email.includes("@")) {
+      newErrors.email = "Invalid email";
+    }
+
+    if (!form.message.trim()) {
+      newErrors.message = "Message is required";
+    }
+
+    setErrors(newErrors);
+
+    // if no errors
+    if (Object.keys(newErrors).length === 0) {
+      console.log("FORM DATA:", form);
+      alert("Form submitted successfully!");
+
+      // reset form
+      setForm({
+        name: "",
+        email: "",
+        message: ""
+      });
+    }
+  }
+
+  return (
+    <div>
+      <h2>Contact Page</h2>
+
+      <form onSubmit={handleSubmit} style={{ maxWidth: "400px" }}>
+        
+        {/* NAME */}
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={form.name}
+            onChange={handleChange}
+          />
+          {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
+        </div>
+
+        {/* EMAIL */}
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+        </div>
+
+        {/* MESSAGE */}
+        <div>
+          <textarea
+            name="message"
+            placeholder="Message"
+            value={form.message}
+            onChange={handleChange}
+          />
+          {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+        </div>
+
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
